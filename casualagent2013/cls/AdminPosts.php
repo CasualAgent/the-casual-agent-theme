d894ed867a0285565d08f9ce91786821dea97d5e
Richard Chong <richardc@criticalmass.com>
2013-11-20 20:02:14 -0700

    full steam ahead start of OOP

---
+++ casualagent2013/cls/AdminPosts.php
@@ -0,0 +1,22 @@
+<?php
+
+
+	class AdminPosts{
+		
+		/*******Public Methods *********/
+		function __construct(){
+			
+		}
+		/*******AJAX Handlers******************/
+		
+		/*******Public Static Methods *********/
+		
+		public static function Init(){
+			
+		}
+		
+		/*******Private Methods *********/
+	}
+	
+	
+?>
